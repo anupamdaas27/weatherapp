6670208eca69d95dff2990bdfe44a5ac8d2831bd
anupamdaas27 <41384140+anupamdaas27@users.noreply.github.com>
2018-12-13 18:21:33 +0000

    fetch issue for IE

---
+++ build/precache-manifest.e280cad2be44e0d46fa6e47ced7bf2dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3ff003d5e29d15febfe",
+    "url": "/weatherapp/forecast/fivedayweather/static/js/runtime~main.c3ff003d.js"
+  },
+  {
+    "revision": "97bc4cfbdb2713ca3c14",
+    "url": "/weatherapp/forecast/fivedayweather/static/js/main.97bc4cfb.chunk.js"
+  },
+  {
+    "revision": "8c248a14f72184b0e1a2",
+    "url": "/weatherapp/forecast/fivedayweather/static/js/1.8c248a14.chunk.js"
+  },
+  {
+    "revision": "97bc4cfbdb2713ca3c14",
+    "url": "/weatherapp/forecast/fivedayweather/static/css/main.6e2f9fb5.chunk.css"
+  },
+  {
+    "revision": "8c248a14f72184b0e1a2",
+    "url": "/weatherapp/forecast/fivedayweather/static/css/1.d74fb735.chunk.css"
+  },
+  {
+    "revision": "8c33aed1df3c0f5ae56cdb4a54175ddc",
+    "url": "/weatherapp/forecast/fivedayweather/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.11513fdf6ddf9170eb72b9a7615b3315.js"
+  "/weatherapp/forecast/fivedayweather/precache-manifest.e280cad2be44e0d46fa6e47ced7bf2dd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/weatherapp/forecast/fivedayweather/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
